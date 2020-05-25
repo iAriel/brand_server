2f9bd3b831415930eb780b0e418fc5def0fe699e
iAriel <ariel@aluno.fapce.edu.br>
2020-05-25 17:09:10 -0300

    Finalizado o controller de pagamento e as rotas

--- src/app/controllers/RecivePaymentDetailsController.js
+++ src/app/controllers/RecivePaymentDetailsController.js
@@ -3,14 +3,51 @@ import { flags } from '../../databases';
 class RecivePaymentDetailsController {
   store(req, res) {
     const {
-      numero_cartão,
       nome_cliente,
-      bandeira,
-      cod_seguranca,
       valor_em_centavos,
       parcelas,
       cod_operadora,
     } = req.body;
+
+    const { brand } = req.params;
+
+    const reciveFlag = flags.find((flag) => flag.Bandeira === brand);
+    if (!reciveFlag) {
+      return res.status(401).json({
+        resposta: 'falha',
+        detalhes: 'Bandeira não autorizada',
+        brand,
+      });
+    }
+
+    const reciveOP = Object.keys(reciveFlag.Operadores).find(
+      (op) => op === cod_operadora
+    );
+
+    if (!reciveFlag.Operadores[reciveOP]) {
+      return res.status(401).json({
+        cod_resposta: 'Operadora-negada',
+        resposta: 'falha',
+        detalhes: 'Operadora sem relação com a bandeira',
+        cod_operadora,
+      });
+    }
+
+    if (reciveFlag.Limite_parcelas < parcelas) {
+      return res.status(401).json({
+        resposta: 'falha',
+        detalhes: 'Limite de parcelas ultrapassado',
+        parcelas_solicitadas: parcelas,
+        limite_parcelas: reciveFlag.Limite_parcelas,
+      });
+    }
+
+    return res.status(200).json({
+      resposta: 'sucesso',
+      nome_cliente,
+      valor_em_centavos,
+      parcelas,
+    });
   }
 }
 
